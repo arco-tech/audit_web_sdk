41855a33d8865956696c1c0bcf1874eb9e9469ed
aydanbrown <mail@aydan.io>
2019-11-06 10:35:31 +1300

    Added ProgressBar colour option

--- src/js/components/ProgressBar.ts
+++ src/js/components/ProgressBar.ts
@@ -2,15 +2,16 @@ import * as m from "mithril";
 
 interface Attrs {
   progress: number;
+  color?: string;
 }
 
 type Vnode = m.Vnode<Attrs>;
 
 export const ProgressBar: m.Component<Attrs> = {
-  view: ({attrs: {progress}}: Vnode) => {
+  view: ({attrs: {progress, color}}: Vnode) => {
     if (progress < 0) { progress = 0; }
     if (progress > 100) { progress = 100; }
-    return m(".progress-bar", [
+    return m(".progress-bar" + (color ? `.bg-${color}` : ""), [
       m(".progress-bar__progress", {style: `width: ${progress}%`}),
     ]);
   },
