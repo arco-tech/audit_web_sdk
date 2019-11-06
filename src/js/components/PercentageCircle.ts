c652e1596de9566dda323de0ebc978a16c589734
aydanbrown <mail@aydan.io>
2019-11-07 06:32:25 +1300

    Added lineWidth option

--- src/js/components/PercentageCircle.ts
+++ src/js/components/PercentageCircle.ts
@@ -3,6 +3,7 @@ import * as m from "mithril";
 interface Attrs {
   percentage: number;
   color?: string;
+  lineWidth: number;
 }
 
 type Vnode = m.Vnode<Attrs>;
@@ -10,22 +11,24 @@ type Vnode = m.Vnode<Attrs>;
 const pi = 3.14159;
 
 export const PercentageCircle: m.Component<Attrs> = {
-  view: ({attrs: {percentage, color}}: Vnode) => {
+  view: ({attrs: {percentage, color, lineWidth=1.5}}: Vnode) => {
     const boxSize = 36;
     const circumference = 100;
     const radius = circumference / (pi * 2);
     const diameter = radius * 2;
     return m("svg.percentage-circle", {viewBox: `0 0 ${boxSize} ${boxSize}`}, [
       m("circle.percentage-circle__background-circle", {
+        style: `stroke-width: ${lineWidth}`,
         cx: boxSize / 2,
         cy: boxSize / 2,
         r: radius,
       }),
       m("path.percentage-circle__line", {
-        style:
-          `stroke-dasharray: ${percentage}, 100; ${
-            color ? `stroke: ${color};` : ""
-          }`,
+        style: [
+          `stroke-width: ${lineWidth}`,
+          `stroke-dasharray: ${percentage}, 100`,
+          color ? `stroke: ${color};` : null,
+        ].filter((style) => style).join(";"),
         d:
           `M${boxSize / 2} ${(boxSize - diameter) / 2} ` +
           `a ${radius} ${radius} 0 0 1 0 ${diameter} ` +
