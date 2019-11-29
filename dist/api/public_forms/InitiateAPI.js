d15fe3123ca217f44dd828dc16f6e5d367220492
aydanbrown <mail@aydan.io>
2019-11-30 07:55:12 +1300

    Moved public_form_initiation/PublicFormInitiationAPI to public_forms/InitiateAPI

---
+++ dist/api/public_forms/InitiateAPI.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var API_1 = require("./API");
+function initiate(initiateToken) {
+    return API_1.request("post", "initiate", {
+        jwt: initiateToken,
+    });
+}
+exports.initiate = initiate;
+//# sourceMappingURL=InitiateAPI.js.map
