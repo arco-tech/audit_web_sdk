da693c20439f5b56beb4125cdbe105c11d2401d6
aydanbrown <mail@aydan.io>
2020-01-13 12:04:00 +1300

    Updated ProgressBar to transition on creation

--- dist/components/ProgressBar.js
+++ dist/components/ProgressBar.js
@@ -2,8 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var m = require("mithril");
 exports.ProgressBar = {
+    oninit: function (_a) {
+        var state = _a.state;
+        state.initialising = true;
+        setTimeout(function () {
+            state.initialising = false;
+            m.redraw();
+        }, 50);
+    },
     view: function (_a) {
-        var _b = _a.attrs, progress = _b.progress, color = _b.color;
+        var _b = _a.attrs, progress = _b.progress, color = _b.color, initialising = _a.state.initialising;
         if (progress < 0) {
             progress = 0;
         }
--- src/js/components/ProgressBar.ts
+++ src/js/components/ProgressBar.ts
@@ -8,7 +8,15 @@ interface Attrs {
 type Vnode = m.Vnode<Attrs>;
 
 export const ProgressBar: m.Component<Attrs> = {
-  view: ({attrs: {progress, color}}: Vnode) => {
+  oninit: ({state}) => {
+    state.initialising = true;
+    setTimeout(() => {
+      state.initialising = false;
+      m.redraw();
+    }, 50);
+  },
+
+  view: ({attrs: {progress, color}, state: {initialising}}: Vnode) => {
     if (progress < 0) { progress = 0; }
     if (progress > 100) { progress = 100; }
     return m(".progress-bar" + (color ? `.bg-${color}` : ""), [
