a559b229c4acd9c4965e94cd979a6c33869504f0
aydanbrown <mail@aydan.io>
2020-05-31 14:20:20 +1200

    Updated form type path

--- dist/api/public/FormTypeAPI.js
+++ dist/api/public/FormTypeAPI.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var FormType_1 = require("../../FormType");
 var API_1 = require("./API");
 function formType(id) {
-    return API_1.request("get", "form-type/" + id)
+    return API_1.request("get", "form-types/" + id)
         .then(function (formTypeJSON) { return new FormType_1.FormType(formTypeJSON); });
 }
 exports.formType = formType;
--- src/js/api/public/FormTypeAPI.ts
+++ src/js/api/public/FormTypeAPI.ts
@@ -2,6 +2,6 @@ import {FormType, FormTypeJSON} from "../../FormType";
 import {request} from "./API";
 
 export function formType(id): Promise<FormType> {
-  return request<FormTypeJSON>("get", "form-type/" + id)
+  return request<FormTypeJSON>("get", "form-types/" + id)
     .then((formTypeJSON: FormTypeJSON) => new FormType(formTypeJSON));
 }
