869a9e5f607c543636755d3589e4536022bb77ce
alexarcombe <arcombe.a@gmail.com>
2021-04-07 09:32:44 +1200

    refactor

--- src/js/PublicForm.ts
+++ src/js/PublicForm.ts
@@ -1,10 +1,6 @@
 export interface PublicFormData {
-  id: number
   named_id: string
   name: string
-  form_type_id: number
-  inserted_at: string
-  updated_at: string
 }
 
 export class PublicForm {
@@ -18,27 +14,11 @@ export class PublicForm {
     return this._data
   }
 
-  public id(): number {
-    return this._data.id
-  }
-
   public namedId(): string {
     return this._data.named_id
   }
 
   public name(): string {
     return this._data.name
-  }
-
-  public formTypeID(): number {
-    return this._data.form_type_id
-  }
-
-  public insertedAt(): Date {
-    return new Date(this._data.inserted_at + "Z")
-  }
-
-  public updatedAt(): Date {
-    return new Date(this._data.updated_at + "Z")
   }
 }
