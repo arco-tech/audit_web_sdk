4d11422c9b1c6b5c2eff0adf2807999440580120
John C Barstow <jcb@jbowtie.geek.nz>
2023-04-15 22:46:03 +1200

    Remove src/js/dist from source control

--- src/js/dist/Environment.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiEndpoint = process.env.API_ENDPOINT;
-//# sourceMappingURL=Environment.js.map
--- src/js/dist/JWT.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function decodePayload(token) {
-    return JSON.parse(atob(token.split(".")[1]));
-}
-exports.decodePayload = decodePayload;
-//# sourceMappingURL=JWT.js.map
